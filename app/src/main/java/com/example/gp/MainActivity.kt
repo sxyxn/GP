3d5d95b8bc13d608e5676c3202e5f5d40f3b0a93
HUNRYEOLLEE <127103398+HUNRYEOLLEE@users.noreply.github.com>
2024-10-17 10:19:00 +0900

    슬라이드 화면 구현

--- app/src/main/java/com/example/gp/MainActivity.kt
+++ app/src/main/java/com/example/gp/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.gp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.widget.ImageView // 추가된 import
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (savedInstanceState == null) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                Log.d("MainActivity", "Navigating to ViewPagerFragment")
+                findViewById<ImageView>(R.id.myImageView).visibility = View.GONE
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ViewPagerFragment())
+                    .commit()
+            }, 2000)
         }
     }
 }
